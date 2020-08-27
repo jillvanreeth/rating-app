6a9374d25db103f43d144b35e8343ef316b19da5
vanreethjill <vanreethjill@gmail.com>
2020-08-27 12:36:12 +0200

    general form styles

--- src/03_molecules/RatingForm/RatingForm.vue
+++ src/03_molecules/RatingForm/RatingForm.vue
@@ -4,13 +4,13 @@
 		<div class="ratingForm__inner">
 			
 			<div class="ratingForm__item">
-				<label class="ratingForm__item__label">Rating</label>
+				<label class="ratingForm__item__label input__label">Rating</label>
 
 				<input v-model.number="theRating" @input="$emit('emitRating', theRating)" class="input__field" name="rating" type="number" min="0" max="5">				
 			</div>
 
 			<div class="ratingForm__item">
-				<label class="ratingForm__item__label">Percentage</label>
+				<label class="ratingForm__item__label input__label">Percentage</label>
 			
 				<input v-model.number="thePercentage" @input="$emit('emitPercentage', thePercentage)" class="input__field" name="percentage" type="number" min="0" max="100">					
 			</div>								
